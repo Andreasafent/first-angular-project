1bdf6934ab09eda094fb8d8233468af1a24ef3cf
Andreasafent <afentoulidisandreas@gmail.com>
2025-01-15 19:06:54 +0200

    Added bootstrap, Routing system, and pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,18 @@
 import { NgFor, NgIf } from '@angular/common';
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { TodoEntryComponent } from './components/todo-entry/todo-entry.component';
 import { TodoAddComponent } from "./components/todo-add/todo-add.component";
+import { HeaderComponent } from "./layout/header/header.component";
+import { FooterComponent } from './layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NgIf, FormsModule, NgFor, TodoEntryComponent, TodoAddComponent],
+  imports: [RouterOutlet, NgIf, FormsModule, NgFor, TodoEntryComponent, TodoAddComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   title = 'My Todo list';
@@ -19,6 +22,10 @@ export class AppComponent {
   ];
   editTodoIndex = -1
 
+  constructor(private cdr: ChangeDetectorRef){
+
+  }
+
   addToTodoList(todo: string){
     if(todo === ""){
       alert("Please enter a task");
@@ -26,6 +33,7 @@ export class AppComponent {
     }
 
     this.todos.push(todo);
+    this.cdr.detectChanges();  // Tell Angular to check for changes
   }
 
   removeTodo(i:number){
